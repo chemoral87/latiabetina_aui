ef57f4dbb140cf939415457f3f6e63a6ab1a0ac0
Sergio Morales <chemoral87@hotmail.com>
2026-02-02 01:43:56 -0600

    drag on circles

--- components/Auditorium/SeatsStageSubsection.vue
+++ components/Auditorium/SeatsStageSubsection.vue
@@ -50,7 +50,13 @@
     <!-- Seats -->
 
     <v-group v-for="seat in seats" :key="seat.id" :config="{ x: seat.x, y: seat.y, draggable: false }">
-      <v-circle :config="Object.assign({}, seat.config, { x: 0, y: 0, listening: true, draggable: false })" @click="handleSeatClick(seat, $event, 'click')" @tap="handleSeatClick(seat, $event, 'tap')" @mousedown="preventDrag" @touchstart="preventDrag" />
+      <v-circle
+        :config="Object.assign({}, seat.config, { x: 0, y: 0, listening: true, draggable: false })"
+        @click="handleSeatClick(seat, $event, 'click')"
+        @tap="handleSeatClick(seat, $event, 'tap')"
+        @mousedown="preventDrag"
+        @touchstart="preventDrag"
+      />
     </v-group>
   </v-group>
 </template>
