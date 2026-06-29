2e303bea4654602d306a30b21b384a595bb64cab
Sergio Morales <chemoral87@hotmail.com>
2026-06-28 19:37:38 -0600

    add classification

--- components/ChurchEvent/classifications.js
+++ components/ChurchEvent/classifications.js
@@ -1,8 +1,9 @@
 export const classifications = [
-  { value: 'general',     label: 'General',     hex: '#9e9e9e' },
+  { value: 'general',     label: 'General',       hex: '#9e9e9e' },
   { value: 'kids',        label: 'Kids',        hex: '#4CAF50' },
-  { value: 'jv3s',        label: 'JV3S',        hex: '#f97316' },
-  { value: 'jv3s-teen',   label: 'JV3S Teen',   hex: '#2196f3' },
+  { value: 'jv3s',         label: 'JV3S',        hex: '#f97316' },
+  { value: 'jv3s-teen',   label: 'JV3S Origen',   hex: '#2196f3' },
+  { value: 'jv3s-impulso',   label: 'JV3S Impulso',   hex: '#195defff' },
   { value: 'jv3s-legado', label: 'JV3S Legado', hex: '#f44336' },
 ]
 
