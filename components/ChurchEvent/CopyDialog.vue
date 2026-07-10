d790dbfbacefe91da116423b87ba02bec4cfda72
Sergio Morales <chemoral87@hotmail.com>
2026-07-10 13:53:00 -0600

    show month when copy

--- components/ChurchEvent/CopyDialog.vue
+++ components/ChurchEvent/CopyDialog.vue
@@ -21,27 +21,13 @@
         </v-btn-toggle>
 
         <template v-if="mode === 'dates'">
-          <v-date-picker
-            v-model="selectedDates"
-            multiple
-            full-width
-            locale="es"
-            :disabled="loading"
-            :events="eventDateArray"
-            event-color="#fb8c00"
-            :allowed-dates="isAllowedDate"
-          />
+          <v-date-picker v-model="selectedDates" multiple full-width locale="es" :disabled="loading"
+            :events="eventDateArray" event-color="#fb8c00" :allowed-dates="isAllowedDate"
+            :picker-date.sync="initialPickerDate" />
 
           <div v-if="selectedDates.length" class="mt-2">
-            <v-chip
-              v-for="date in sortedDates"
-              :key="date"
-              small
-              class="mr-1 mb-1"
-              close
-              :disabled="loading"
-              @click:close="removeDate(date)"
-            >
+            <v-chip v-for="date in sortedDates" :key="date" small class="mr-1 mb-1" close :disabled="loading"
+              @click:close="removeDate(date)">
               {{ date | moment("DD MMM YYYY") }}
             </v-chip>
           </div>
@@ -50,43 +36,21 @@
         <template v-else>
           <v-row dense>
             <v-col cols="12" sm="6">
-              <v-menu
-                v-model="startDateMenu"
-                :close-on-content-click="false"
-                transition="scale-transition"
-                offset-y
-                min-width="auto"
-              >
+              <v-menu v-model="startDateMenu" :close-on-content-click="false" transition="scale-transition" offset-y
+                min-width="auto">
                 <template #activator="{ on, attrs }">
-                  <v-text-field
-                    v-model="recurrence.start_date"
-                    label="Fecha inicial"
-                    prepend-icon="mdi-calendar"
-                    readonly
-                    v-bind="attrs"
-                    v-on="on"
-                  />
+                  <v-text-field v-model="recurrence.start_date" label="Fecha inicial" prepend-icon="mdi-calendar"
+                    readonly v-bind="attrs" v-on="on" />
                 </template>
                 <v-date-picker v-model="recurrence.start_date" locale="es" @input="startDateMenu = false" />
               </v-menu>
             </v-col>
             <v-col cols="12" sm="6">
-              <v-menu
-                v-model="endDateMenu"
-                :close-on-content-click="false"
-                transition="scale-transition"
-                offset-y
-                min-width="auto"
-              >
+              <v-menu v-model="endDateMenu" :close-on-content-click="false" transition="scale-transition" offset-y
+                min-width="auto">
                 <template #activator="{ on, attrs }">
-                  <v-text-field
-                    v-model="recurrence.end_date"
-                    label="Fecha final"
-                    prepend-icon="mdi-calendar"
-                    readonly
-                    v-bind="attrs"
-                    v-on="on"
-                  />
+                  <v-text-field v-model="recurrence.end_date" label="Fecha final" prepend-icon="mdi-calendar" readonly
+                    v-bind="attrs" v-on="on" />
                 </template>
                 <v-date-picker v-model="recurrence.end_date" locale="es" @input="endDateMenu = false" />
               </v-menu>
@@ -95,13 +59,7 @@
 
           <label class="caption grey--text text--darken-1">Días de la semana</label>
           <v-chip-group v-model="recurrence.days_of_week" multiple column>
-            <v-chip
-              v-for="(day, index) in weekDays"
-              :key="day"
-              filter
-              outlined
-              :value="index"
-            >
+            <v-chip v-for="(day, index) in weekDays" :key="day" filter outlined :value="index">
               {{ day }}
             </v-chip>
           </v-chip-group>
@@ -136,11 +94,13 @@ export default {
   },
 
   data() {
+    const today = this.$moment().format('YYYY-MM-DD')
+
     return {
       mode: 'dates',
       selectedDates: [],
       recurrence: {
-        start_date: null,
+        start_date: today,
         end_date: null,
         days_of_week: [],
       },
@@ -165,6 +125,10 @@ export default {
       }
 
       return !!this.recurrence.start_date && !!this.recurrence.end_date && this.recurrence.days_of_week.length > 0
+    },
+
+    initialPickerDate() {
+      return this.churchEvent.event_date ? this.churchEvent.event_date.substring(0, 7) : this.$moment().format('YYYY-MM')
     },
   },
 
